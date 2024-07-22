ddd9e17b669692d15776110bfb8fd67bc058921d
Shreyas Kulkarni <30172640+shreyask21@users.noreply.github.com>
2024-07-23 02:47:09 +0530

    Initial Release

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+
+namespace Keep_Asleep
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport(@"User32", SetLastError = true, EntryPoint = "RegisterPowerSettingNotification",
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr RegisterPowerSettingNotification(IntPtr hRecipient, ref Guid PowerSettingGuid,
+            Int32 Flags);
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        internal struct POWERBROADCAST_SETTING
+        {
+            public Guid PowerSetting;
+            public uint DataLength;
+            public byte Data;
+        }
+
+        Guid GUID_LIDSWITCH_STATE_CHANGE = new Guid(0xBA3E0F4D, 0xB817, 0x4094, 0xA2, 0xD1, 0xD5, 0x63, 0x79, 0xE6, 0xA0, 0xF3);
+        const int DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000;
+        const int WM_POWERBROADCAST = 0x0218;
+        const int PBT_POWERSETTINGCHANGE = 0x8013;
+
+        private bool isLaptopLidOpen = true;
+        private uint num_sleep = 0;
+
+        private void OnPowerBroadcast(IntPtr wParam, IntPtr lParam)
+        {
+            if ((int)wParam == PBT_POWERSETTINGCHANGE)
+            {
+                POWERBROADCAST_SETTING ps = (POWERBROADCAST_SETTING)Marshal.PtrToStructure(lParam, typeof(POWERBROADCAST_SETTING));
+                if (ps.PowerSetting == GUID_LIDSWITCH_STATE_CHANGE)
+                {
+                    isLaptopLidOpen = ps.Data != 0;
+                    if (isLaptopLidOpen)
+                    {
+                        UpdateTrayText();
+                    }
+                }
+            }
+        }
+
+        void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Collapsed;
+            RegisterForPowerNotifications();
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(hwnd).AddHook(new HwndSourceHook(WndProc));
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void RegisterForPowerNotifications()
+        {
+            IntPtr handle = new WindowInteropHelper(Application.Current.Windows[0]).Handle;
+            IntPtr hLIDSWITCHSTATECHANGE = RegisterPowerSettingNotification(handle,
+                 ref GUID_LIDSWITCH_STATE_CHANGE,
+                 DEVICE_NOTIFY_WINDOW_HANDLE);
+        }
+
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_POWERBROADCAST:
+                    OnPowerBroadcast(wParam, lParam);
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void UpdateTrayText()
+        {
+            TBIconTBox.Text = "Keep Asleep V"
+                + Assembly.GetExecutingAssembly().GetName().Version.ToString()
+                + "\r\nRe-Sleep Count: "
+                + num_sleep.ToString();
+        }
+        [STAThread]
+        private void ReSleep_Thread()
+        {
+            Thread.CurrentThread.IsBackground = true;
+            while (true)
+            {
+                if (isLaptopLidOpen)
+                {
+                    Thread.Sleep(3000);
+                }
+                else
+                {
+                    Thread.Sleep(2000);
+                    // Wait a bit and recheck if user just opened the lid
+                    // Recovery mode--if user holds down insert key
+                    if (!isLaptopLidOpen)
+                    {
+                        num_sleep++;
+                        // Turn off display -> retrigger modern standby
+                        SendMessage(0xFFFF, 0x112, 0xF170, 2);
+                    }
+                }
+            }
+        }
+        public MainWindow()
+        {
+            WindowState = WindowState.Minimized;
+            InitializeComponent();
+            
+            this.SourceInitialized += MainWindow_SourceInitialized;
+            UpdateTrayText();
+            
+            new Thread(() => ReSleep_Thread()).Start();
+        }
+        private void ExitBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# keep_asleep
-Fixes Windows Modern Standby Wake Up
+# Keep Asleep!
+Fixes Windows 10/11's "Modern Standby" random wake up issues on Laptops.  
+Also prevents system wake up if mouse gets bumped when the lid is closed.  
+
+## Quick Installation
+* Download the exe from releases section
+* Open Windows Explorer and paste the following address in the address bar at the top:  
+   `%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup`
+* Copy and paste the downloaded exe in the above folder
+
+## Methodology
+This app hooks into the windows API for lid sensor and keeps the track of the sensor   
+If the laptop wakes up from sleep and the sensor says the lid is closed,  
+this app will re trigger the modern standby to enter sleep by sending a system message.
